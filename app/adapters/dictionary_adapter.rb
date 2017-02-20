0d17db8fc46b2c3c8a46e60b4e714d762ad3c21a
krissasantos <krissa.santos31@gmail.com>
2017-02-20 18:11:36 -0500

    assigned button ids with each letter int he alphabet to is corresponding letter

--- app/adapters/dictionary_adapter.rb
+++ app/adapters/dictionary_adapter.rb
@@ -9,12 +9,5 @@ class DictionaryAdapter
      converted_response = response.parsed_response.split(" ").sample
   end
 
-
-  # def self.get_word(user)
-  #    response = self.get("http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words?difficulty=#{user.set_difficulty}&maxLength=#{user.set_maxLength}")
-  #    converted_response = response.parsed_response.split(" ").sample
-  # end
 end
-# byebug
-# p "hola"
 
