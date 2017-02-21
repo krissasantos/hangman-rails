a855ee5c4c0acfff093b71118ea637ba83ef6ed4
krissasantos <krissa.santos31@gmail.com>
2017-02-21 04:44:53 -0500

    split selected letters into misses and guesses

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,41 +3,89 @@ class Game < ApplicationRecord
   # attr_accessor :selected_word
   # attr_accessor :selected_letters
   @@selected_letters = []
-  MAX_FAILED_ATTEMPTS = 6
+  @@misses = []
+  @@guesses = []
+
+
+
+  MAX_MISSES = 6
 
   def selected_letters
     @@selected_letters
   end
 
-
-  def failed_attempts
-    self.selected_letters.select { |letter|
-      !self.word.include?(letter)
-    }.size
-
+  def misses
+    @@misses
   end
+
+  def guesses
+    @@guesses
+  end
+
+
+
+  # def misses
+  #   self.selected_letters.select { |letter|
+  #     !self.word.include?(letter)
+  #   }
+
+  # end
+
+  # def guesses
+  #   self.selected_letters.select {|letter|
+  #     self.word.include?(letter)
+  #   } #select is a private method error. 
+  # end
+
 
   def guessed?
     (self.word.split('') - selected_letters).empty?
   end
 
   def finished?
-    failed_attempts >= MAX_FAILED_ATTEMPTS || guessed?
+    self.misses.size >= MAX_MISSES || guessed?
   end
 
+
+  def blanks
+     "____ " * self.word.size
+  
+  end
+
+
+
+  # def update_blanks(letter)
+  #   if self.selected_letters == []
+  #     self.blanks
+  #   else
+  #     self.word.split("").map.with_index do |char, idx|
+  #       if char == letter.downcase
+  #         self.blanks[idx] = letter
+  #         byebug
+  #       elsif char != letter.downcase
+  #         self.blanks[idx] = "____ " 
+  #       end
+  #     end
+  #   end
+    
+  #   self.blanks
+  # end
+
   def select!(letter)
-    # raise GameOverError if finished?
     # byebug
     #cannot get passed this because selected_letters is nil!
-     if !self.selected_letters.include?(letter) && !self.word.include?(letter.downcase)
-        self.selected_letters << letter 
-    end
+      if !self.misses.include?(letter) && !self.word.include?(letter.downcase)
+        self.misses << letter 
+      elsif !self.guesses.include?(letter) && self.word.include?(letter.downcase)
+        self.guesses << letter 
+      end
 
-    # self.word.include? letter
+    # update_blanks(letter)
   end
 
 
 end
 
  
+   # if !self.selected_letters.include?(letter) && !self.word.include?(letter.downcase)
 
