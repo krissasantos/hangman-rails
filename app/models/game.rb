17f5d49a42ddb3cac60a66b9ea02bec1cb94b7c5
krissasantos <krissa.santos31@gmail.com>
2017-02-21 02:53:41 -0500

    added logic to selecting letters

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,35 +3,35 @@ class Game < ApplicationRecord
   # attr_accessor :selected_word
   # attr_accessor :selected_letters
   @@selected_letters = []
-
-  # def initialize
-  
-  # end
+  MAX_FAILED_ATTEMPTS = 6
 
   def selected_letters
     @@selected_letters
   end
 
 
-  # def failed_attempts
-  #   selected_letters.select { |letter|
-  #     !word.include?(letter)
-  #   }.size
-  # end
+  def failed_attempts
+    self.selected_letters.select { |letter|
+      !self.word.include?(letter)
+    }.size
+  end
 
-  # def guessed?
-  #   (word.split('') - selected_letters).empty?
-  # end
+  def guessed?
+    (word.split('') - selected_letters).empty?
+  end
 
-  # def finished?
-  #   failed_attempts >= MAX_FAILED_ATTEMPTS || guessed?
-  # end
+  def finished?
+    failed_attempts >= MAX_FAILED_ATTEMPTS || guessed?
+  end
 
   def select!(letter)
     # raise GameOverError if finished?
     byebug
     #cannot get passed this because selected_letters is nil!
-    self.selected_letters << letter unless self.selected_letters.include? letter
+     if !self.selected_letters.include?(letter) && !self.word.include?(letter.downcase)
+        self.selected_letters << letter 
+    end
+
     # self.word.include? letter
   end
 
